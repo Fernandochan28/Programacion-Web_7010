d4609f80ec57a2b99fa77eb16df8563fab537365
Fernandochan28 <7010@itescam.edu.mx>
2023-09-22 16:36:10 -0600

    Actividades de clase

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home",loadChildren:()=>import("./modules/home/home.module").then(m=>m.HomeModule)},
+  {path:"Cliente",loadChildren:()=>import("./modules/cliente/cliente.module").then(c=>c.ClienteModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
